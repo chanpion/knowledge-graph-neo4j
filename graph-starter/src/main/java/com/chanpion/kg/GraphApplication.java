fd113482e6f068ff3cefcce019940bb59db79bf8
April.Chen <forchpeng@163.com>
2021-01-29 16:37:27 +0800

    Initial commit

---
+++ graph-starter/src/main/java/com/chanpion/kg/GraphApplication.java
@@ -0,0 +1,16 @@
+package com.chanpion.kg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author April Chen
+ * @date 2021/1/22 13:39
+ */
+@SpringBootApplication
+public class GraphApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GraphApplication.class, args);
+    }
+}
---
+++ graph-web/src/main/java/com/chanpion/kg/web/GraphController.java
@@ -0,0 +1,12 @@
+package com.chanpion.kg.web;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author April Chen
+ * @date 2021/1/22 13:42
+ */
+@RestController
+public class GraphController {
+
+}
